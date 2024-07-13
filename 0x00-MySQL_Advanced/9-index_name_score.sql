5f43c4817e1de1b67055744cc2bcc9ccaffe08f9
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-13 00:36:01 +0000

    task9

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
--- creates an index idx_name_first_score
--- on the table names and the first letter of name and the score.
+-- script that creates a function SafeDiv that divides (and returns) the first
+-- by the second number or returns 0 if the second number is equal to 0
 CREATE INDEX idx_name_first_score
 ON names(name(1), score);
